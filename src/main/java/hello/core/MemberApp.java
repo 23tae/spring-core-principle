5ad801387d55bfe783a33a9e17df01da97722b5f
23tae <taehoonkim.dev@gmail.com>
2024-07-05 11:52:39 +0900

    Fix: appconfig를 통해 객체 생성

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,9 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
         Member newMember = new Member(1L, "memberA", Grade.VIP);
         memberService.join(newMember);
 
